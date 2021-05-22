767c62fbbf2612f770ac7c0a8404b64b9cdd52b9
KondeU <370242479@qq.com>
2021-05-22 12:11:46 +0800

    build_msvc构建脚本中加入拷贝License文件
    Desc: 拷贝License文件。
    Type: Feature
    Link: NA

--- script/build_msvc.bat
+++ script/build_msvc.bat
@@ -99,4 +99,11 @@ copy ..\dependency\zeromq\zeromq\bin\libzmq.dll libraries /Y
 rem [### Copy USER CMakeLists file ###]
 copy ..\..\script\TMSS.CMakeLists.txt .\CMakeLists.txt /Y
 
+rem [### Copy LICENSE files ###]
+mkdir licenses
+copy ..\..\3rd\zeromq\COPYING.LESSER licenses\zeromq.txt /Y
+copy ..\..\3rd\cppzmq\LICENSE licenses\cppzmq.txt /Y
+copy ..\..\3rd\cereal\LICENSE licenses\cereal.txt /Y
+copy ..\..\LICENSE .\License.txt /Y
+
 cd ..\..
