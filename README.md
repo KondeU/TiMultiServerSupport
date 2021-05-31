7226e48419efb0d3fef5e733924e0cc84c309ef6
KondeU <370242479@qq.com>
2021-06-01 01:07:00 +0800

    仓库重命名为TiRPC，调整构建脚本
    Desc: 仓库重命名为TiRPC，调整构建脚本。
    Type: Feature
    Link: NA

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# TiServiceFramework
- 
+# TiRPC
+
+Histories:
+
+| TiRPC                | N/A                                      |
+| -------------------- | ---------------------------------------- |
+| TiRPC                | https://github.com/KondeU/TiRPC          |
+| TiMultiServerSupport | https://github.com/KondeU/TiMultiServerSupport |
+| TiServiceFramework   | https://github.com/KondeU/TiServiceFramework |
+
--- script/ci.bat
+++ script/ci.bat
@@ -1,4 +1,18 @@
 cd %~dp0
-
+call clean.bat
+mkdir ..\artifact\msvc_x64
 call build_msvc.bat Release x64 VS2015
-rem call build_mingw.bat
+xcopy build\archive artifact\msvc_x64 /E /Y
+rem build_msvc will change the working path to .\..
+
+cd %~dp0
+call clean.bat
+mkdir ..\artifact\msvc_x86
+call build_msvc.bat Release x86 VS2015
+xcopy build\archive artifact\msvc_x86 /E /Y
+rem build_msvc will change the working path to .\..
+
+cd %~dp0
+call clean.bat
+call build_mingw.bat
+rem build_mingw will change the working path to .\..
