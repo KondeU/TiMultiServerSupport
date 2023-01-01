ac1d1c45be4891824ce76cdc13dc12b813e5d131
Deu.K <370242479@qq.com>
2023-01-01 15:37:49 +0800

    Update ci.bat

--- script/ci.bat
+++ script/ci.bat
@@ -1,14 +1,14 @@
 cd %~dp0
 call clean.bat
 mkdir ..\artifact\msvc_x64
-call build_msvc.bat Release x64 VS2015
+call build_msvc.bat Release x64 VS2022
 xcopy build\archive artifact\msvc_x64 /E /Y
 rem build_msvc will change the working path to .\..
 
 cd %~dp0
 call clean.bat
 mkdir ..\artifact\msvc_x86
-call build_msvc.bat Release x86 VS2015
+call build_msvc.bat Release x86 VS2022
 xcopy build\archive artifact\msvc_x86 /E /Y
 rem build_msvc will change the working path to .\..
 
